24502097faae34e0d16831d9be8dc8bcf31c4b1a
JonathanDev1 <160975307+JonathanDev1@users.noreply.github.com>
2025-05-19 20:18:24 -0300

    Add files via upload

---
+++ Programação de Computadores/atividades praticas de Programação de computadores/repeticao_while.py
@@ -0,0 +1,10 @@
+# Inicializa a variavel contador com o valor 1
+contador = 1
+
+# Enquanto o valor do contador for menor ou igual a 5, o bloco abaixo sera executado
+while contador <= 5:
+    # Imprime o valor atual do contador
+    print(contador)
+
+    # Incrementa o valor do contador em 1
+    contador += 1  # mesma coisa que contador = contador + 1
---
+++ Programação de Computadores/atividades praticas de Programação de computadores/sequencial.py
@@ -0,0 +1,11 @@
+# Solicita ao usuario que digite o primeiro numero
+numero1 = float(input("Digite o primeiro numero: "))
+
+# Solicita ao usuario que digite o segundo numero
+numero2 = float(input("Digite o segundo numero: "))
+
+# Calcula a soma dos dois numeros
+soma = numero1 + numero2
+
+# Exibe o resultado da soma na tela
+print("A soma e:", soma)
